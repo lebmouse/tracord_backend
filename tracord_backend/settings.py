9c1734e7435169e34486f115aba2c48e8bf07107
lebmouse <gtah2yk@gmail.com>
2019-03-07 16:32:47 +0900

    static 오류 해걸

--- tracord_backend/settings.py
+++ tracord_backend/settings.py
@@ -122,11 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files
 MEDIA_URL = '/upload_files/'
